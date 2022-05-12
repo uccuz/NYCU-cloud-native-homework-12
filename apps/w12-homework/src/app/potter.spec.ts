739385e42eb7e701a3be5dcee847ee45e89c041a
uccuz <justin2000721@gmail.com>
2022-05-12 15:40:55 +0800

    create one book test

--- apps/w12-homework/src/app/potter.spec.ts
+++ apps/w12-homework/src/app/potter.spec.ts
@@ -5,3 +5,9 @@ test('test no book', () => {
   potter.buyBook([]);
   expect(potter.calPrice).toBe(0);
 });
+
+test('test one book', () => {
+  const potter = new Potter();
+  potter.buyBook([1]);
+  expect(potter.calPrice).toBe(8);
+});
