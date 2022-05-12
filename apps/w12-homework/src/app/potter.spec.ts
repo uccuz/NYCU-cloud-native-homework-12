0a925033bc5159dd8a521dee229b31aa4a8d0ba8
uccuz <justin2000721@gmail.com>
2022-05-12 16:15:22 +0800

    create potter basic test

--- apps/w12-homework/src/app/potter.spec.ts
+++ apps/w12-homework/src/app/potter.spec.ts
@@ -22,6 +22,29 @@ describe('Potter', () => {
     expect(potter.calPrice).toBe(8);
   });
 
+  test('one of book2', () => {
+    potter.buyBook([1]);
+    expect(potter.calPrice).toBe(8);
+  });
 
+  test('one of book3', () => {
+    potter.buyBook([2]);
+    expect(potter.calPrice).toBe(8);
+  });
+
+  test('one of book4', () => {
+    potter.buyBook([3]);
+    expect(potter.calPrice).toBe(8);
+  });
+
+  test('one of book5', () => {
+    potter.buyBook([4]);
+    expect(potter.calPrice).toBe(8);
+  });
+
+  test('three of book1', () => {
+    potter.buyBook([1,1,1]);
+    expect(potter.calPrice).toBe(8*3);
+  });
 
 });
